93f5ae2f07c69d362ee4c24cda4a1e8db6210449
NadavMozeson <nadavson02@gmail.com>
2024-06-17 13:33:13 +0300

    Finished refactor of machine_precision.py

--- Machine Precision/machine_precision.py
+++ Machine Precision/machine_precision.py
@@ -14,11 +14,14 @@ def get_machine_epsilon():
     return eps
 
 
+def run_on_expression(expr):
+    print(f"\nResult of {expr} :")
+    eval_expr = eval(expr)
+    print_with_colors(bcolors.FAIL, f"before using machine epsilon: {format(eval_expr)}")
+    print_with_colors(bcolors.OKGREEN, f"After correcting with machine epsilon: {format(eval_expr - epsilon)}")
+
 if __name__ == '__main__':
     epsilon = get_machine_epsilon()
     print_with_colors(bcolors.OKBLUE, f"Machine Precision  : {epsilon}")
+    run_on_expression("abs(3.0 * (4.0 / 3.0 - 1.0) - 1.0)")
 
-    expression = abs(3.0 * (4.0 / 3.0 - 1.0) - 1.0)
-    print("\nResult of abs(3.0 * (4.0 / 3.0 - 1.0) - 1.0) :")
-    print_with_colors(bcolors.FAIL, f"before using machine epsilon: {format(expression)}")
-    print_with_colors(bcolors.OKGREEN, f"After correcting with machine epsilon: {format(expression - epsilon)}")
